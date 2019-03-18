095844124dcefe0a79317373070c7289d723271a
Kamal <40141015+byte0111@users.noreply.github.com>
2019-03-18 13:55:28 +0200

    update dashboard

--- resources/views/admin/layouts/AdminBase.blade.php
+++ resources/views/admin/layouts/AdminBase.blade.php
@@ -34,11 +34,9 @@
                         <!-- Logo icon -->
                     <b class="logo-icon p-l-10">
 
-                        <img src="{{asset('images/admin/icons/logo-icon.png')}}" alt="homepage" class="light-logo" />
-
                     </b>
                     <span class="logo-text">
-                             <img src="{{asset('images/admin/icons/logo-text.png')}}" alt="homepage" class="light-logo" />
+                             EDGE <span style="color:#f82249" >HOME+</span>
                         </span>
                 </a>
                 <a class="topbartoggler d-block d-md-none waves-effect waves-light" href="javascript:void(0)" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><i class="ti-more"></i></a>
@@ -84,39 +82,161 @@
                             <li class="sidebar-item"><a href="form-wizard.html" class="sidebar-link"><i class="mdi mdi-note-plus"></i><span class="hide-menu"> Devices1 </span></a></li>
                         </ul>
                     </li>
-                    <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="pages-buttons.html" aria-expanded="false"><i class="mdi mdi-relative-scale"></i><span class="hide-menu">Buttons</span></a></li>
-                    <li class="sidebar-item"> <a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="mdi mdi-face"></i><span class="hide-menu">Icons </span></a>
-                        <ul aria-expanded="false" class="collapse  first-level">
-                            <li class="sidebar-item"><a href="icon-material.html" class="sidebar-link"><i class="mdi mdi-emoticon"></i><span class="hide-menu"> Material Icons </span></a></li>
-                            <li class="sidebar-item"><a href="icon-fontawesome.html" class="sidebar-link"><i class="mdi mdi-emoticon-cool"></i><span class="hide-menu"> Font Awesome Icons </span></a></li>
-                        </ul>
-                    </li>
-                    <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="pages-elements.html" aria-expanded="false"><i class="mdi mdi-pencil"></i><span class="hide-menu">Elements</span></a></li>
-                    <li class="sidebar-item"> <a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="mdi mdi-move-resize-variant"></i><span class="hide-menu">Addons </span></a>
-                        <ul aria-expanded="false" class="collapse  first-level">
-                            <li class="sidebar-item"><a href="index2.html" class="sidebar-link"><i class="mdi mdi-view-dashboard"></i><span class="hide-menu"> Dashboard-2 </span></a></li>
-                            <li class="sidebar-item"><a href="pages-gallery.html" class="sidebar-link"><i class="mdi mdi-multiplication-box"></i><span class="hide-menu"> Gallery </span></a></li>
-                            <li class="sidebar-item"><a href="pages-calendar.html" class="sidebar-link"><i class="mdi mdi-calendar-check"></i><span class="hide-menu"> Calendar </span></a></li>
-                            <li class="sidebar-item"><a href="pages-invoice.html" class="sidebar-link"><i class="mdi mdi-bulletin-board"></i><span class="hide-menu"> Invoice </span></a></li>
-                            <li class="sidebar-item"><a href="pages-chat.html" class="sidebar-link"><i class="mdi mdi-message-outline"></i><span class="hide-menu"> Chat Option </span></a></li>
-                        </ul>
-                    </li>
-                    <li class="sidebar-item"> <a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="mdi mdi-account-key"></i><span class="hide-menu">Authentication </span></a>
-                        <ul aria-expanded="false" class="collapse  first-level">
-                            <li class="sidebar-item"><a href="authentication-login.html" class="sidebar-link"><i class="mdi mdi-all-inclusive"></i><span class="hide-menu"> Login </span></a></li>
-                            <li class="sidebar-item"><a href="authentication-register.html" class="sidebar-link"><i class="mdi mdi-all-inclusive"></i><span class="hide-menu"> Register </span></a></li>
-                        </ul>
-                    </li>
                 </ul>
             </nav>
             <!-- End Sidebar navigation -->
         </div>
     </aside>
+    <div class="page-wrapper">
+        <!-- ============================================================== -->
+        <!-- Bread crumb and right sidebar toggle -->
+        <!-- ============================================================== -->
+        <div class="page-breadcrumb">
+            <div class="row">
+                <div class="col-12 d-flex no-block align-items-center">
+                    <h4 class="page-title">Dashboard</h4>
+                    <div class="ml-auto text-right">
+                        <nav aria-label="breadcrumb">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                                <li class="breadcrumb-item active" aria-current="page">Library</li>
+                            </ol>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- ============================================================== -->
+        <!-- End Bread crumb and right sidebar toggle -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- Container fluid  -->
+        <!-- ============================================================== -->
+        <div class="container-fluid">
+            <!-- ============================================================== -->
+            <!-- Sales Cards  -->
+            <!-- ============================================================== -->
+            <div class="row">
+                <!-- Column -->
+                <div class="col-md-6 col-lg-2 col-xlg-3">
+                    <div class="card card-hover">
+                        <div class="box bg-cyan text-center">
+                            <h1 class="font-light text-white"><i class="mdi mdi-roomba"></i></h1>
+                            <h6 class="text-white">Living Room</h6>
+                        </div>
+                    </div>
+                </div>
+                <!-- Column -->
+                <div class="col-md-6 col-lg-4 col-xlg-3">
+                    <div class="card card-hover">
+                        <div class="box bg-success text-center">
+                            <h1 class="font-light text-white"><i class="mdi mdi-food-variant"></i></h1>
+                            <h6 class="text-white">Kitchen</h6>
+                        </div>
+                    </div>
+                </div>
+                <!-- Column -->
+                <div class="col-md-6 col-lg-2 col-xlg-3">
+                    <div class="card card-hover">
+                        <div class="box bg-warning text-center">
+                            <h1 class="font-light text-white"><i class="zmdi zmdi-washing-machine"></i></h1>
+                            <h6 class="text-white">Wash Room</h6>
+                        </div>
+                    </div>
+                </div>
+                <!-- Column -->
+                <div class="col-md-6 col-lg-2 col-xlg-3">
+                    <div class="card card-hover">
+                        <div class="box bg-danger text-center">
+                            <h1 class="font-light text-white"><i class="zmdi zmdi-desktop-windows"></i></h1>
+                            <h6 class="text-white">Office</h6>
+                        </div>
+                    </div>
+                </div>
+                <!-- Column -->
+                <div class="col-md-6 col-lg-2 col-xlg-3">
+                    <div class="card card-hover">
+                        <div class="box bg-info text-center">
+                            <h1 class="font-light text-white"><i class="mdi mdi-lock-open"></i></h1>
+                            <h6 class="text-white">Home Door</h6>
+                        </div>
+                    </div>
+                </div>
+                <!-- Column -->
+            </div>
+            <!-- ============================================================== -->
+            <!-- Sales chart -->
+            <!-- ============================================================== -->
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-md-flex align-items-center">
+                                <div>
+                                    <h4 class="card-title">Site Analysis</h4>
+                                    <h5 class="card-subtitle">Overview of Latest Month</h5>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <!-- column -->
+                                <div class="col-lg-9">
+                                    <div class="flot-chart">
+                                        <div class="flot-chart-content" id="flot-line-chart"></div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-3">
+                                    <div class="row">
+                                        <div class="col-6">
+                                            <div class="bg-dark p-10 text-white text-center">
+                                                <i class="fa fa-user m-b-5 font-16"></i>
+                                                <h5 class="m-b-0 m-t-5">22</h5>
+                                                <small class="font-light">Temprature</small>
+                                            </div>
+                                        </div>
+                                        <div class="col-6">
+                                            <div class="bg-dark p-10 text-white text-center">
+                                                <i class="fa fa-plus m-b-5 font-16"></i>
+                                                <h5 class="m-b-0 m-t-5">120</h5>
+                                                <small class="font-light">Hume</small>
+                                            </div>
+                                        </div>
+                                        <div class="col-12 m-t-15">
+                                            <div class="bg-dark p-10 text-white text-center">
+                                                <i class="fa fa-cart-plus m-b-5 font-16"></i>
+                                                <h5 class="m-b-0 m-t-5">No Warring</h5>
+                                                <small class="font-light">PIR System</small>
+                                            </div>
+                                        </div>
+                                        <div class="col-12 m-t-15">
+                                            <div class="bg-dark p-10 text-white text-center">
+                                                <i class="fa fa-tag m-b-5 font-16"></i>
+                                                <h5 class="m-b-0 m-t-5">No Warring</h5>
+                                                <small class="font-light">Alert System</small>
+                                            </div>
+                                        </div>
+                                        <div class="col-12 m-t-15">
+                                            <div class="bg-dark p-10 text-white text-center">
+                                                <i class="fa fa-table m-b-5 font-16"></i>
+                                                <h5 class="m-b-0 m-t-5">Close</h5>
+                                                <small class="font-light">Door</small>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                                <!-- column -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
     <footer class="footer text-center">
         All Rights Reserved by  Designed and Developed by <a href="https://wrappixel.com">RandomByte</a>.
     </footer>
-</div>
 <script src="{{asset('js/admin/jquery.min.js')}}"></script>
 <script src="{{asset('js/admin/jquery.ui.touch-punch-improved.js')}}"></script>
 <script src="{{asset('js/admin/jquery-ui.min.js')}}"></script>
